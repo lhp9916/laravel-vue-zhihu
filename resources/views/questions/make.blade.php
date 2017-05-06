69c151d524e1c0b7d7b4c59edd6847fbc3a0a1a9
lhp9916 <lhp9916@gmail.com>
2017-05-06 23:53:31 +0800

    美化编辑器

--- resources/views/questions/make.blade.php
+++ resources/views/questions/make.blade.php
@@ -20,8 +20,9 @@
                             </div>
 
                             <div class="form-group {{ $errors->has('body')? 'has-error':''}}">
+                                <label for="body">描述</label>
                                 <!-- 编辑器容器 -->
-                                <script id="container" name="body" type="text/plain">
+                                <script id="container" name="body" style="height:200px;" type="text/plain">
                                     {!! old('body') !!}
                                 </script>
                                 @if($errors->has('body'))
@@ -38,7 +39,17 @@
     </div>
     <!-- 实例化编辑器 -->
     <script type="text/javascript">
-        var ue = UE.getEditor('container');
+        var ue = UE.getEditor('container', {
+            toolbars: [
+                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link', 'insertimage', 'fullscreen']
+            ],
+            elementPathEnabled: false,
+            enableContextMenu: false,
+            autoClearEmptyNode: true,
+            wordCount: false,
+            imagePopup: false,
+            autotypeset: {indent: true, imageBlockLine: 'center'}
+        });
         ue.ready(function () {
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
