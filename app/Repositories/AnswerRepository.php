8e3c77e3d5e712cbb4a05d10e26d19ccd2038c35
lhp9916 <lhp9916@gmail.com>
2017-05-13 00:13:32 +0800

    实现提交答案

---
+++ app/Repositories/AnswerRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Answer;
+
+class AnswerRepository
+{
+    public function create(array $data)
+    {
+        return Answer::create($data);
+    }
+}
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('vendor.ueditor.assets')
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -28,6 +29,70 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        {{ $question->answers_count }}个答案
+                    </div>
+
+                    <div class="panel-body">
+
+                        @foreach($question->answers as $answer)
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="">
+                                        <img width="36" src="{{ $answer->user->avatar }}"
+                                             alt="{{ $answer->user->name }}">
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">
+                                        <a href="/user/{{ $answer->user->name }}">{{ $answer->user->name }}</a>
+                                    </h4>
+                                    {!! $answer->body !!}
+                                </div>
+                            </div>
+                        @endforeach
+
+
+                        <form action="/questions/{{$question->id}}/answer" method="post">
+                            {!! csrf_field() !!}
+
+                            <div class="form-group {{ $errors->has('body')? 'has-error':''}}">
+                                <!-- 编辑器容器 -->
+                                <script id="container" name="body" type="text/plain">
+                                    {!! old('body') !!}
+                                </script>
+                                @if($errors->has('body'))
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                @endif
+                            </div>
+                            <button class="btn btn-success pull-right" type="submit">提交答案</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
+@section('js')
+    <!-- 实例化编辑器 -->
+    <script type="text/javascript">
+        var ue = UE.getEditor('container', {
+            toolbars: [
+                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft', 'justifycenter', 'justifyright', 'link', 'insertimage', 'fullscreen']
+            ],
+            elementPathEnabled: false,
+            enableContextMenu: false,
+            autoClearEmptyNode: true,
+            wordCount: false,
+            imagePopup: false,
+            autotypeset: {indent: true, imageBlockLine: 'center'}
+        });
+        ue.ready(function () {
+            ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+        });
+    </script>
+@endsection
 @endsection
