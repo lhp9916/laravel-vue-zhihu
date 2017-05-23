0c55fd8c55a22aea8e82577d4b6a13000f15eefa
lhp9916 <lhp9916@gmail.com>
2017-05-23 16:59:55 +0800

    小修改

--- app/Mailer/Mailer.php
+++ app/Mailer/Mailer.php
@@ -12,11 +12,11 @@ class Mailer
      * @param $email
      * @param array $data
      */
-    public function sendTo($template, $email, array $data)
+    protected function sendTo($template, $email, array $data)
     {
         $content = new SendCloudTemplate($template, $data);
 
-        Mail::raw($template, function ($message) use ($email) {
+        Mail::raw($content, function ($message) use ($email) {
             $message->from('lhp9916@gmail.com', 'lhp');
             $message->to($email);
         });
